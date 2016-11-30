72611de292f037e146428eaf2731630ad17f1fb7
Yogesh Werulkar <werulkaryogesh@gmail.com>
2016-11-30 11:10:42 +0530

    project

---
+++ FoodAndClothParadies/app/src/main/java/com/interactionone/interviewtask/foodandclothparadies/ScreenSlidePageFragment.java
@@ -0,0 +1,42 @@
+package com.interactionone.interviewtask.foodandclothparadies;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+import com.interactionone.interviewtask.foodandclothparadies.models.ProductImages;
+
+import java.util.ArrayList;
+
+public class ScreenSlidePageFragment extends android.support.v4.app.Fragment {
+    ProductImages images = new ProductImages();
+    private ImageLoader imageLoader= ProductListingActivity.getInstance().getImageLoader();
+    public static ScreenSlidePageFragment getNewInstance(ProductImages images){
+        ScreenSlidePageFragment screenSlidePageFragment=new ScreenSlidePageFragment();
+        screenSlidePageFragment.images=images;
+return screenSlidePageFragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_screen_slide_page, container, false);
+      NetworkImageView networkImageView=(NetworkImageView) v.findViewById(R.id.textviewImagesq);
+        if (imageLoader==null)
+            imageLoader= ProductListingActivity.getInstance().getImageLoader();
+        networkImageView.setImageUrl(images.getImageUrl(), imageLoader);
+
+
+        return v;
+    }
+}
+
